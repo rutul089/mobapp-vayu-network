701301e36295c7674143aec0f6739eadc66fd120
rutul089@gmail.com <rutul089@gmail.com>
2025-06-29 11:13:51 +0530

    setup: initial project setup with dependencies

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ alignItems: 'center', flex: 1, justifyContent: 'center' }}>
+      <Text>Vayu Netowrk</Text>
+    </View>
+  );
+}
